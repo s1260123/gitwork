3e4d2d957e9bf05ac74ec630c57faa3333e8592a
屋仲駿 <yanakashun@MacBook-Pro.local>
2019-07-11 07:43:19 +0900

    greeding

--- dice.cpp
+++ dice.cpp
@@ -6,6 +6,12 @@ using namespace std;
 
 int main(){
   int a = 0,b = 0;
+  string name;
+
+  cout << "what is your name?" << "\n";
+  cout << ">";
+  cin >> name;
+  cout << "Hello, " << name << "!\n";
 
   srand(time(NULL));
   a = rand() % 6 + 1;
