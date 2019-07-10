df6bb7857d2021f5ff94d98800019b41811aa22a
屋仲駿 <yanakashun@MacBook-Pro.local>
2019-07-11 08:03:45 +0900

    add

--- dice.cpp
+++ dice.cpp
@@ -5,23 +5,26 @@
 using namespace std;
 
 int main(){
-  int a = 0,b = 0;
+  int a = 0,b = 0,i = 0;
   string name;
 
-  cout << "what is your name?" << "\n";
-  cout << ">";
-  cin >> name;
-  cout << "Hello, " << name << "!\n";
+  while(i < 2){
+    cout << "what is your name?" << "\n";
+    cout << ">";
+    cin >> name;
+    cout << "Hello, " << name << "!\n";
 
-  srand(time(NULL));
-  a = rand() % 6 + 1;
-  b = rand() % 6 + 1;
-  cout << "Die 1: " << a << "\n";
-  cout << "Die 2: " << b << "\n";
-  cout << "Total value: " << a + b << "\n";
+    srand(time(NULL));
+    a = rand() % 6 + 1;
+    b = rand() % 6 + 1;
+    cout << "Die 1: " << a << "\n";
+    cout << "Die 2: " << b << "\n";
+    cout << "Total value: " << a + b << "\n";
 
-  if(a + b > 7) cout << name << ",Won!\n";
-  else cout << name << ",Lost!\n";
+    if(a + b > 7) cout << name << ",Won!\n";
+    else cout << name << ",Lost!\n";
+    i++;
+  }
 
   return 0;
 }
