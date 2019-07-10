c64b45430388ac0a1a9b9f136883dfaf8e80f8a3
屋仲駿 <yanakashun@MacBook-Pro.local>
2019-07-11 07:25:07 +0900

    greeding

---
+++ greed.cpp
@@ -0,0 +1,14 @@
+#include  <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  string name;
+
+  cout << "what is your name?" << "\n";
+  cout << ">";
+  cin >> name;
+  cout << "Hello, " << name << "!\n";
+
+  return 0;
+}
